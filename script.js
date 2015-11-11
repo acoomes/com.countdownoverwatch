f47137ca9d7a57fe8fb3f45a5e07a43b21951197
acoomes <86drift@gmail.com>
2015-11-10 17:11:13 -0800

    Initial Timer Function
    Added initial timer function (currently shows days).
    Set up DOM to inject timer output from script, removed <p> tag on
    welcome, tweaked styling.
    Next - floor day count, add hours, minutes, and seconds.

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  document.getElementById("timer").innerHTML = daysUntil() + " days";
+}
+var daysUntil = function() {
+  var now = Date.now();
+  var launch = Date.UTC(2016, 6, 21, 0, 0, 0);
+  var millis = launch - now;
+  var days = millis / (1000*60*60*24);
+  return days;
+}
