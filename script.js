4b19d0f8d643a51b07050d6ed85d3d230c502a04
acoomes <86drift@gmail.com>
2015-11-19 11:27:20 -0800

    Added getMillis & comments.
    Refactored the date calls and millisecond timer calculation into it's
    own function, getMillis. Added comments to all functions.

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
+/* Wait for the DOM to load, then run getCountdown every 0.5 seconds.
+ */
 window.onload = function() {
   setInterval(getCountdown, 500);
 }
-var daysUntil = function() {
+/* Subtract the current number of milliseconds (from epoch) from the number
+ * of milliseconds as of June 21, 2016 (Overwatch release).
+ */
+var getMillis = function() {
   var now = new Date();
   var launch = new Date(2016, 5, 21, 0, 0, 0);
   var millis = launch.getTime() - now.getTime();
-  var days = millis / (1000*60*60*24);
+  return millis;
+}
+/* Convert milliseconds into days.
+ * Return the floored number of days.
+ */
+var daysUntil = function() {
+  var days = getMillis() / (1000*60*60*24);
   return Math.floor(days);
 }
+/* Convert milliseconds into hours.
+ * Return the number of hours, rounded up to the nearest integer.
+ */
 var hoursUntil = function() {
-  var now = new Date();
-  var launch = new Date(2016, 5, 21, 0, 0, 0);
-  var millis = launch.getTime() - now.getTime();
-  var hours = millis / (1000*60*60);
+  var hours = getMillis() / (1000*60*60);
   return Math.ceil(hours % 24);
 }
+/* Convert milliseconds into minutes.
+ * Return the floored number of minutes.
+ */
 var minUntil = function() {
-  var now = new Date();
-  var launch = new Date(2016, 5, 21, 0, 0, 0);
-  var millis = launch.getTime() - now.getTime();
-  var minutes = millis / (1000*60);
+  var minutes = getMillis() / (1000*60);
   return Math.floor(minutes % 60);
 }
+/* Convert milliseconds into seconds.
+ * Return the floored number of seconds, rounded to the nearest integer.
+ */
 var secUntil = function() {
-  var now = new Date();
-  var launch = new Date(2016, 5, 21, 0, 0, 0);
-  var millis = launch.getTime() - now.getTime();
-  var seconds = millis / (1000);
+  var seconds = getMillis() / (1000);
   return Math.round(seconds % 60);
 }
+/* Fill in the timer divs with the days, hours, minutes, and seconds.
+ */
 var getCountdown = function() {
   document.getElementById("days").innerHTML = daysUntil() + " days ";
   document.getElementById("hours").innerHTML = hoursUntil() + " hours ";
