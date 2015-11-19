f7fc7fd0f19c54c755b26ef32ab8a8e48fa897a0
Andrew Coomes <86drift@gmail.com>
2015-11-18 22:09:49 -0800

    Finished timer function
    Fixed the day count and added functions for hours, minutes, and seconds.
    Also added setInterval for automatic counting.

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 window.onload = function() {
-  document.getElementById("timer").innerHTML = daysUntil() + " days";
+  setInterval(getCountdown, 500);
 }
 var daysUntil = function() {
-  var now = Date.now();
-  var launch = Date.UTC(2016, 6, 21, 0, 0, 0);
-  var millis = launch - now;
+  var now = new Date();
+  var launch = new Date(2016, 5, 21, 0, 0, 0);
+  var millis = launch.getTime() - now.getTime();
   var days = millis / (1000*60*60*24);
-  return Math.floor(days);
+  return Math.round(days);
+}
+var hoursUntil = function() {
+  var now = new Date();
+  var launch = new Date(2016, 5, 21, 0, 0, 0);
+  var millis = launch.getTime() - now.getTime();
+  var hours = millis / (1000*60*60);
+  return Math.round(hours % 24);
+}
+var minUntil = function() {
+  var now = new Date();
+  var launch = new Date(2016, 5, 21, 0, 0, 0);
+  var millis = launch.getTime() - now.getTime();
+  var minutes = millis / (1000*60);
+  return Math.round(minutes % 60);
+}
+var secUntil = function() {
+  var now = new Date();
+  var launch = new Date(2016, 5, 21, 0, 0, 0);
+  var millis = launch.getTime() - now.getTime();
+  var seconds = millis / (1000);
+  return Math.round(seconds % 60);
+}
+var getCountdown = function() {
+  document.getElementById("timer").innerHTML = daysUntil() + " days " + hoursUntil() + " hours " + minUntil() + " minutes " + secUntil() + " seconds";
 }
