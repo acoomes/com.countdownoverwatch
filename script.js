0979ca5a84200a0e2aabcb7436c51339feb00203
acoomes <86drift@gmail.com>
2015-11-19 15:56:25 -0800

    Renaming & Styling
    Renamed functions, broke out div structure, added some styling.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 window.onload = function() {
   setInterval(getCountdown, 500);
 }
+
 /* Subtract the current number of milliseconds (from epoch) from the number
  * of milliseconds as of June 21, 2016 (Overwatch release).
  */
@@ -12,39 +13,45 @@ var getMillis = function() {
   var millis = launch.getTime() - now.getTime();
   return millis;
 }
+
 /* Convert milliseconds into days.
  * Return the floored number of days.
  */
-var daysUntil = function() {
+var getDays = function() {
   var days = getMillis() / (1000*60*60*24);
   return Math.floor(days);
 }
+
 /* Convert milliseconds into hours.
  * Return the number of hours, rounded up to the nearest integer.
  */
-var hoursUntil = function() {
+var getHrs = function() {
   var hours = getMillis() / (1000*60*60);
   return Math.ceil(hours % 24);
 }
+
 /* Convert milliseconds into minutes.
  * Return the floored number of minutes.
  */
-var minUntil = function() {
+
+var getMins = function() {
   var minutes = getMillis() / (1000*60);
   return Math.floor(minutes % 60);
 }
+
 /* Convert milliseconds into seconds.
  * Return the floored number of seconds, rounded to the nearest integer.
  */
-var secUntil = function() {
+var getSecs = function() {
   var seconds = getMillis() / (1000);
-  return Math.round(seconds % 60);
+  return Math.floor(seconds % 60);
 }
+
 /* Fill in the timer divs with the days, hours, minutes, and seconds.
  */
 var getCountdown = function() {
-  document.getElementById("days").innerHTML = daysUntil() + " days ";
-  document.getElementById("hours").innerHTML = hoursUntil() + " hours ";
-  document.getElementById("minutes").innerHTML = minUntil() + " minutes ";
-  document.getElementById("seconds").innerHTML = secUntil() + " seconds";
+  document.getElementById("numDays").innerHTML = getDays();
+  document.getElementById("numHours").innerHTML = getHrs();
+  document.getElementById("numMinutes").innerHTML = getMins();
+  document.getElementById("numSeconds").innerHTML = getSecs();
 }
